dba6c32bcf3ab4a2a6f80c412a59d62d4a782b18
Dominic <dominic.cocchiarella@gmail.com>
2017-04-05 15:30:42 -0700

    add font icon.

---
+++ src/Styles/SetStyles.js
@@ -0,0 +1,101 @@
+import React from 'react'
+import Radium, { Style } from 'radium'
+import { Normalize, GlobalTheme } from '../index'
+
+const getFonts = (assetsUrl) => {
+  return (
+    `@font-face {
+      font-family: 'ic-icons';
+      src: url('${assetsUrl}/Fonts/ic-icons.eot');
+      src: url('${assetsUrl}/ic-icons.eot?#iefix') format('embedded-opentype'),
+           url('${assetsUrl}/ic-icons.woff') format('woff'),
+           url('${assetsUrl}/ic-icons.ttf') format('truetype'),
+           url('${assetsUrl}/ic-icons.svg#ic-icons') format('svg');
+      font-weight: normal;
+      font-style: normal;
+    }
+
+    @font-face {
+      font-family: 'Open Sans';
+      font-weight: 300;
+      font-style: normal;
+      src: font-url('${assetsUrl}/OpenSans-Light/OpenSans-Light.eot');
+      src: font-url('${assetsUrl}/OpenSans-Light/OpenSans-Light.eot?#iefix') format('embedded-opentype'),
+           local('Open Sans Light'),
+           local('OpenSans-Light'),
+           url('${assetsUrl}/OpenSans-Light/OpenSans-Light.woff') format('woff'),
+           url('${assetsUrl}/OpenSans-Light/OpenSans-Light.ttf') format('truetype'),
+           url('${assetsUrl}/OpenSans-Light/OpenSans-Light.svg#OpenSans') format('svg');
+    }
+
+    @font-face {
+      font-family: 'Open Sans';
+      font-weight: 400;
+      font-style: normal;
+      src: url('${assetsUrl}/OpenSans/OpenSans.eot');
+      src: url('${assetsUrl}/OpenSans/OpenSans.eot?#iefix') format('embedded-opentype'),
+           local('Open Sans'),
+           local('OpenSans'),
+           url('${assetsUrl}/OpenSans/OpenSans.woff') format('woff'),
+           url('${assetsUrl}/OpenSans/OpenSans.ttf') format('truetype'),
+           url('${assetsUrl}/OpenSans/OpenSans.svg#OpenSans') format('svg');
+    }
+
+    @font-face {
+      font-family: 'Open Sans';
+      font-weight: 600;
+      font-style: normal;
+      src: url('${assetsUrl}/OpenSans-Semibold/OpenSans-Semibold.eot');
+      src: url('${assetsUrl}/OpenSans-Semibold/OpenSans-Semibold.eot?#iefix') format('embedded-opentype'),
+           local('Open Sans Semibold'),
+           local('OpenSans-Semibold'),
+           url('${assetsUrl}/OpenSans-Semibold/OpenSans-Semibold.woff') format('woff'),
+           url('${assetsUrl}/OpenSans-Semibold/OpenSans-Semibold.ttf') format('truetype'),
+           url('${assetsUrl}/OpenSans-Semibold/OpenSans-Semibold.svg#OpenSans') format('svg');
+    }
+
+    @font-face {
+      font-family: 'Open Sans';
+      font-weight: 700;
+      font-style: normal;
+      src: url('${assetsUrl}/OpenSans-Bold/OpenSans-Bold.eot');
+      src: url('${assetsUrl}/OpenSans-Bold/OpenSans-Bold.eot?#iefix') format('embedded-opentype'),
+           local('Open Sans Bold'),
+           local('OpenSans-Bold'),
+           url('${assetsUrl}/OpenSans-Bold/OpenSans-Bold.woff') format('woff'),
+           url('${assetsUrl}/OpenSans-Bold/OpenSans-Bold.ttf') format('truetype'),
+           url('${assetsUrl}/OpenSans-Bold/OpenSans-Bold.svg#OpenSans') format('svg');
+    }`
+  )
+}
+
+let fontLoaded = false
+
+const writeFonts = (assetsUrl) => {
+  // if on server, just return
+  if (typeof window === 'undefined') { return }
+
+  const style = document.createElement('style')
+  style.setAttribute('type', 'text/css')
+  style.innerHTML = getFonts(assetsUrl)
+  document.head.appendChild(style)
+}
+
+const Styles = React.createClass({
+  propTypes: {
+    assetsUrl: React.PropTypes.string.isRequired
+  },
+
+  componentWillMount() {
+    if (fontLoaded) { return }
+
+    writeFonts(this.props.assetsUrl)
+    fontLoaded = true
+  },
+
+  render() {
+    return <Style rules={Object.assign({}, Normalize, GlobalTheme)} />
+  }
+})
+
+export default Radium(Styles)
--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import Icon from './Components/Icon/Icon'
 import NavigationPills from './Components/NavigationPills/NavigationPills'
 import Normalize from './Styles/Normalize'
 import GlobalTheme from './Styles/GlobalTheme'
+import SetStyles from './Styles/SetStyles'
 
 export {
   GlobalTheme,
   Icon,
   NavigationPills,
-  Normalize
+  Normalize,
+  SetStyles
 }
