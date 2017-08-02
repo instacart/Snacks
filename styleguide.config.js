b291a34d2bcf3a7c017d935bbad65b6e04c9a896
Dominic <dominic.cocchiarella@gmail.com>
2017-08-01 20:13:11 -0700

    Instacart theme for docs.

--- styleguide.config.js
+++ styleguide.config.js
@@ -9,5 +9,37 @@ module.exports = {
     '**/*/*hexValues.js'
   ],
   showCode: true,
-  showUsage: true
+  showUsage: true,
+  styles: {
+    StyleGuide: {
+      content: {
+        maxWidth: '1600px'
+      }
+    }
+  },
+  theme: {
+    color: {
+      base: '#333',
+      light: '#999',
+      lightest: '#ccc',
+      link: '#43B02A',
+      linkHover: '#43B02A',
+      border: '#BDBDBD',
+      name: '#FF467E',
+      type: '#5FCA44',
+      error: '#fff',
+      baseBackground: '#fff',
+      errorBackground: '#c00',
+      codeBackground: '#f7f7f7',
+      sidebarBackground: '#f7f7f7',
+    },
+    fontFamily: {
+      base: [
+        'Open Sans',
+        'Helvetica Neue',
+        'Helvetica',
+        'sans-serif'
+      ]
+    }
+  }
 }
