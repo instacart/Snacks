6a17d74245b33b6fb12fb2472cc8fce43babce8a
Dominic <dominic.cocchiarella@gmail.com>
2017-08-08 14:26:48 -0700

    Adding script to guide build for fonts.

---
+++ scripts/moveBuildAssets.js
@@ -0,0 +1,10 @@
+// This file moves any static assets, like fonts, to the /docs/build folder.
+// These assets will then be used by the public styleguide - https://instacart.github.io/Snacks/
+const fs = require('fs-extra')
+
+console.log('Copying static assets...')
+
+fs.copy('src/fonts/','docs/build/src/fonts', err => {
+  if (err) { return console.error(err) }
+  console.log('Copied fonts')
+})
