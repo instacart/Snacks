7bdd32fdcde3387749f0a7d1bdc83de9d984a274
Nicholas <nicholas.wargnier@instacart.com>
2017-12-19 11:32:51 -0800

    update snapshots

--- src/components/Menus/DropdownMenu.js
+++ src/components/Menus/DropdownMenu.js
@@ -2,7 +2,6 @@ import React     from 'react'
 import PropTypes from 'prop-types'
 import Radium    from 'radium'
 import Menu      from './Menu'
-import zIndex    from '../../styles/zIndex'
 
 const styles = {
   menuContainer: {
