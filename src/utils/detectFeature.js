00ed3381a9c447bb21b83f790a23e2345b5ca430
Dominic <dominic.cocchiarella@gmail.com>
2018-05-17 10:18:40 -0700

    actually fix issue

--- src/utils/detectFeature.js
+++ src/utils/detectFeature.js
@@ -1,15 +1,18 @@
 import _ from 'underscore'
 
 export const supportsCSSGrid = _.memoize(() => {
-  const isNodeEnv = typeof window === 'undefined'
-  const userAgent = (isNodeEnv && navigator && navigator.userAgent) || ''
-  const isTestEnv = userAgent.match(/(Node.js|jsdom)/)
-
-  if (isTestEnv || isNodeEnv) { return true }
+  if (isNodeEnv() || isTestEnv()) { return true }
 
   const elm = document.createElement('div')
 
   return elm.style['grid-template-rows'] !== undefined
 })
 
-export default { supportsCSSGrid }
+export const isNodeEnv = () => typeof window === 'undefined'
+
+export const isTestEnv = () => {
+  const userAgent = (window && window.navigator && navigator.userAgent) || ''
+  return userAgent.match(/(Node.js|jsdom)/)
+}
+
+export default { isNodeEnv, isTestEnv, supportsCSSGrid }
