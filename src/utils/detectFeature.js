319d555f465ed1cbfa4cfc652faa3db039ac9767
Dominic Cocchiarella <dominic.cocchiarella@gmail.com>
2018-05-18 16:44:11 -0700

    Fix css grid detection (#154)
    * fix how we detect css grid support
    
    * actually fix issue

--- src/utils/detectFeature.js
+++ src/utils/detectFeature.js
@@ -1,15 +1,18 @@
 import _ from 'underscore'
 
 export const supportsCSSGrid = _.memoize(() => {
-  const userAgent = (navigator && navigator.userAgent) || ''
-  const isTestEnv = userAgent.match(/(Node.js|jsdom)/)
-  const isNodeEnv = typeof window === 'undefined'
-
-  if (isTestEnv || isNodeEnv) { return true }
+  if (isNodeEnv() || isTestEnv()) { return true }
 
   const elm = document.createElement('div')
 
   return elm.style['grid-template-rows'] !== undefined
 })
 
-export default { supportsCSSGrid }
+export const isNodeEnv = () => typeof window === 'undefined'
+
+export const isTestEnv = () => {
+  const userAgent = (window && window.navigator && navigator.userAgent) || ''
+  return userAgent.match(/(Node.js|jsdom)/)
+}
+
+export default { isNodeEnv, isTestEnv, supportsCSSGrid }
