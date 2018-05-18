de496ccdd5e4313453267c32ec20eff9ea69a9af
spencerschack <spencer.schack@instacart.com>
2018-05-18 13:17:35 -0700

    exporter

---
+++ icon_exporter.rb
@@ -0,0 +1,57 @@
+#! /usr/bin/env ruby
+require 'pathname'
+require 'nokogiri'
+
+name = ARGV.first
+
+unless name
+  puts <<-HELP
+INSTRUCTIONS
+./icon_export_helper.rb ICON_NAME
+When asked to copy the svg, open the Design System Sketch file, right-click on the icon you want to export, and select "Copy SVG Code" and the script will take care of the rest.
+  HELP
+  exit
+end
+
+root = Pathname.new(__FILE__).dirname.expand_path
+path = root.join('src', 'components', 'Icon', 'icons', "#{name}.svg")
+initial_copy = `pbpaste`
+source = nil
+
+if path.exist?
+  puts "#{name} file already exists, overwrite? (y/n)"
+  exit unless STDIN.gets.strip.downcase == 'y'
+end
+
+if initial_copy.strip.start_with?('<svg')
+  puts "SVG already in clipboard, use it for #{name}? (y/n)"
+  if STDIN.gets.strip.downcase == 'y'
+    source = initial_copy
+  end
+end
+
+unless source
+  puts "Copy svg for #{name} please"
+  loop do
+    source = `pbpaste`
+    if source != initial_copy && source.strip.start_with?('<svg')
+      break
+    end
+    sleep 0.25
+  end
+end
+
+document = Nokogiri::XML.parse(source)
+svg = document.css('svg')
+svg
+  .set(:width, '24px')
+  .set(:height, '24px')
+  .set(:viewBox, '0 0 24 24')
+svg = svg.first
+element = document.css('path').first
+element.remove_attribute('id')
+svg.content = ''
+element.parent = svg
+path.write("#{svg}\n")
+
+puts "Done!"
