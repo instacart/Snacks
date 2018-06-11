accea8bf4744ecfb8689f0468d1538971f657858
Will Faurot <wfaurot@gmail.com>
2018-06-11 10:32:03 -0700

    Responsive helpers (#153)

---
+++ src/styles/__tests__/responsive.spec.js
@@ -0,0 +1,70 @@
+import responsive, { breakpoints } from '../responsive'
+
+const { up, down, only, between } = responsive
+
+describe('up', () => {
+  it('returns a media query that matches the given screen size or larger', () => {
+    const breakpointNames = Object.keys(breakpoints)
+
+    breakpointNames.forEach(size => {
+      expect(up(size)).toEqual(`@media (min-width: ${breakpoints[size].min}px)`)
+    })
+  })
+})
+
+describe('down', () => {
+  it('ignores xl as an argument because it has no upper bound', () => {
+    expect(() => down('xl')).toThrow()
+  })
+
+  it('returns a media query that matches the given screen size or smaller', () => {
+    const breakpointNames = Object.keys(breakpoints).filter(b => b !== 'xl')
+
+    breakpointNames.forEach(size => {
+      expect(down(size)).toEqual(`@media (max-width: ${breakpoints[size].max}px)`)
+    })
+  })
+})
+
+describe('only', () => {
+  it('works for the smallest size', () => {
+    expect(only('xs')).toEqual(`@media (max-width: ${breakpoints.xs.max}px)`)
+  })
+
+  it('works for the largest size', () => {
+    expect(only('xl')).toEqual(`@media (min-width: ${breakpoints.xl.min}px)`)
+  })
+
+  it('works for every size that has both a lower/upper bound', () => {
+    const breakpointNames = ['sm', 'md', 'mdLg', 'lg']
+
+    breakpointNames.forEach(size => {
+      expect(only(size)).toEqual(`@media (min-width: ${breakpoints[size].min}px) and (max-width: ${breakpoints[size].max}px)`)
+    })
+  })
+})
+
+describe('between', () => {
+  it('returns a media query that matches screens between each given size\'s minimum value', () => {
+    const testCases = [
+      ['xs', 'md'],
+      ['sm', 'md'],
+      ['sm', 'xl'],
+      ['md', 'xl']
+    ]
+
+    testCases.forEach(t => {
+      const [ lower, upper ] = t
+      const expected = `@media (min-width: ${breakpoints[lower].min}px) and (max-width: ${breakpoints[upper].min}px)`
+
+      expect(between(lower, upper)).toEqual(expected)
+    })
+  })
+})
+
+it('throws an error when invalid screen sizes are given', () => {
+  expect(() => up('not')).toThrow()
+  expect(() => down('a')).toThrow()
+  expect(() => only('valid')).toThrow()
+  expect(() => between('screen', 'size')).toThrow()
+})
--- src/styles/responsive.js
+++ src/styles/responsive.js
@@ -1,10 +1,10 @@
-const COLUMN_WIDTH =    104               // 1/2 Item Card width
+const COLUMN_WIDTH = 104                  // 1/2 Item Card width
 
-const SCREEN_SM_MIN =   768               // 6 columns rounded
-const SCREEN_MD_MIN =   COLUMN_WIDTH * 8  // 832
+const SCREEN_SM_MIN = 768                 // 6 columns rounded
+const SCREEN_MD_MIN = COLUMN_WIDTH * 8    // 832
 const SCREEN_MDLG_MIN = COLUMN_WIDTH * 10 // 1040
-const SCREEN_LG_MIN =   COLUMN_WIDTH * 12 // 1248
-const SCREEN_XL_MIN =   COLUMN_WIDTH * 14 // 1456 (max width)
+const SCREEN_LG_MIN = COLUMN_WIDTH * 12   // 1248
+const SCREEN_XL_MIN = COLUMN_WIDTH * 14   // 1456 (max width)
 
 const SCREEN_XS_MAX = SCREEN_SM_MIN - 1   // 767
 const SCREEN_SM_MAX = SCREEN_MD_MIN - 1   // 831
@@ -12,19 +12,102 @@ const SCREEN_MD_MAX = SCREEN_MDLG_MIN - 1 // 1039
 const SCREEN_MDLG_MAX = SCREEN_LG_MIN - 1 // 1247
 const SCREEN_LG_MAX = SCREEN_XL_MIN - 1   // 1455
 
+export const breakpoints = {
+  xs: {
+    min: 0,
+    max: SCREEN_XS_MAX
+  },
+  sm: {
+    min: SCREEN_SM_MIN,
+    max: SCREEN_SM_MAX
+  },
+  md: {
+    min: SCREEN_MD_MIN,
+    max: SCREEN_MD_MAX
+  },
+  mdLg: {
+    min: SCREEN_MDLG_MIN,
+    max: SCREEN_MDLG_MAX
+  },
+  lg: {
+    min: SCREEN_LG_MIN,
+    max: SCREEN_LG_MAX
+  },
+  xl: {
+    min: SCREEN_XL_MIN
+  }
+}
+
+const assertValidSizes = (...sizes) => {
+  sizes.forEach(size => {
+    if (breakpoints[size] === undefined) {
+      throw new Error(`Screen size(s) ${sizes.join(', ')} not supported. Must be one of ${Object.keys(breakpoints).join(', ')}`) // eslint-disable-line
+    }
+  })
+}
+
+const up = (size) => {
+  assertValidSizes(size)
+
+  return `@media (min-width: ${breakpoints[size].min}px)`
+}
+
+const down = (size) => {
+  if (size === 'xl') {
+    throw new Error('size "xl" not supported')
+  }
+
+  assertValidSizes(size)
+
+  return `@media (max-width: ${breakpoints[size].max}px)`
+}
+
+const only = (size) => {
+  assertValidSizes(size)
+
+  const { min, max } = breakpoints[size]
+
+  if (!min) {
+    return `@media (max-width: ${max}px)`
+  }
+
+  if (!max) {
+    return `@media (min-width: ${min}px)`
+  }
+
+  return `@media (min-width: ${min}px) and (max-width: ${max}px)`
+}
+
+const between = (lowerSize, upperSize) => {
+  assertValidSizes(lowerSize, upperSize)
+
+  const lower = breakpoints[lowerSize].min
+  const upper = breakpoints[upperSize].min
+
+  return `@media (min-width: ${lower}px) and (max-width: ${upper}px)`
+}
+
 export default {
-  xs:   `@media (max-width: ${SCREEN_XS_MAX}px)`,
-  sm:   `@media (min-width: ${SCREEN_SM_MIN}px) and (max-width: ${SCREEN_SM_MAX}px)`,
-  md:   `@media (min-width: ${SCREEN_MD_MIN}px) and (max-width: ${SCREEN_MD_MAX}px)`,
+  xs: `@media (max-width: ${SCREEN_XS_MAX}px)`,
+  sm: `@media (min-width: ${SCREEN_SM_MIN}px) and (max-width: ${SCREEN_SM_MAX}px)`,
+  md: `@media (min-width: ${SCREEN_MD_MIN}px) and (max-width: ${SCREEN_MD_MAX}px)`,
   mdLg: `@media (min-width: ${SCREEN_MDLG_MIN}px) and (max-width: ${SCREEN_MDLG_MAX}px)`,
-  lg:   `@media (min-width: ${SCREEN_LG_MIN}px) and (max-width: ${SCREEN_LG_MAX}px)`,
-  xl:   `@media (min-width: ${SCREEN_XL_MIN}px)`,
+  lg: `@media (min-width: ${SCREEN_LG_MIN}px) and (max-width: ${SCREEN_LG_MAX}px)`,
+  xl: `@media (min-width: ${SCREEN_XL_MIN}px)`,
   columnWidth: COLUMN_WIDTH,
   screenWidths: {
     sm: COLUMN_WIDTH * 6,
     md: SCREEN_MD_MIN,
     mdLg: SCREEN_MDLG_MIN,
     lg: SCREEN_LG_MIN,
-    xl: SCREEN_XL_MIN,
-  }
+    xl: SCREEN_XL_MIN
+  },
+
+  // New breakpoint helpers. These do everything the existing
+  // breakpoints do and are more flexible, so they should eventually
+  // replace the individual size exports.
+  up,
+  down,
+  only,
+  between
 }
