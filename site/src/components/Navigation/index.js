2d6d35b76d60252183b93201e4e5ef4b7f561c51
Logan Murdock <logan@instacart.com>
2018-06-27 14:35:23 -0700

    default group to active false

--- site/src/components/Navigation/index.js
+++ site/src/components/Navigation/index.js
@@ -2,8 +2,8 @@ import React from 'react'
 import styles from './styles'
 import NavGroup from './NavGroup'
 
-export default ({ children, style }) =>
+export default ({ children }) =>
   <div style={styles.container}>
-    <NavGroup title='Foundations' />
+    <NavGroup title='Foundations' isActive={false} />
     {children}
   </div>
