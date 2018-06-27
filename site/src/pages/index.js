2b4c7b4257abaf815fe51990c465b04101355149
spencerschack <spencer.schack@instacart.com>
2018-06-27 12:30:44 -0700

    live editor

--- site/src/pages/index.js
+++ site/src/pages/index.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import {
+  LiveProvider,
+  LiveEditor,
+  LiveError,
+  LivePreview
+} from 'react-live'
+import {Button} from 'ic-snacks'
 
 import Layout from '../components/layout'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <LiveProvider code="<Button />" scope={{Button}}>
+      <LiveEditor />
+      <LiveError />
+      <LivePreview />
+    </LiveProvider>
   </Layout>
 )
 
