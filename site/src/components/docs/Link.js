ea9e44070f9ddaceea6a4f37be3184a1027a7174
Collin <collin.ferry@gmail.com>
2018-06-28 14:02:49 -0700

    added link.js

---
+++ site/src/components/docs/Link.js
@@ -0,0 +1,33 @@
+import React, {Fragment} from 'react'
+import Playground from '../Playground'
+
+export default () => (
+
+<Fragment>
+The `Link` component is valuable mainly because it's hooked up to Themer.
+The link's resting/hover colors will match the current theme's action and actionHover values.
+
+  <Playground>
+    {`
+  <div>
+    <div style={{marginBottom: '16px'}}>
+      <Link href="/radish">Normal link</Link>
+    </div>
+
+    <div style={{marginBottom: '16px'}}>
+      <Link href="/radish" onClick={(e, props) => { e.preventDefault() }}>With an onClick callback</Link>
+    </div>
+
+    <div style={{marginBottom: '16px'}}>
+      <Link href="/radish" onClick={(e, props) => { e.preventDefault() }} elementAttributes={{'aria-label': 'Foo'}}>With extra attributes like an aria-label</Link>
+    </div>
+
+    <div style={{marginBottom: '16px'}}>
+      <Link href="/radish" onClick={(e, props) => { e.preventDefault() }} style={{textDecoration: 'underline', fontWeight: 600}}>With added styles</Link>
+    </div>
+  </div>
+  `}
+  </Playground>
+</Fragment>
+
+)
