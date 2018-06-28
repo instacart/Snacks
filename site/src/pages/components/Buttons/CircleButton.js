ac83b1fbce6f4f1afda04ec2b8771d2a03536446
Collin <collin.ferry@gmail.com>
2018-06-27 17:03:46 -0700

    converted Buttons .md files

---
+++ site/src/pages/components/Buttons/CircleButton.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Layout from '../../../components/Layout'
+import Playground from '../../../components/Playground'
+import {Title2, Title3} from '../../../components/Typography'
+
+export default () => (
+<Layout>
+
+CircleButton example:
+
+<Playground>
+  {`
+    <div>
+      <CircleButton>+</CircleButton>
+    </div>
+  `}
+</Playground
+
+</Layout>
+)
