c46ef407f0dbae08f4e25a2353877d185710618c
Collin <collin.ferry@gmail.com>
2018-06-28 14:08:43 -0700

    added menu docs

---
+++ site/src/components/docs/DropdownMenu.js
@@ -0,0 +1,33 @@
+import React, {Fragment} from 'react'
+import Playground from '../Playground'
+
+export default () => (
+
+  <Fragment>
+  DropdownMenu Example:
+
+  <Playground>
+    {`
+<div style={{display: 'flex'}}>
+  <div style={{marginRight: '20px'}}>
+    <DropdownMenu triggerElement={<Button> Share </Button>}>
+      <MenuItem label="Share via Email" value="email" leftIcon="emailFilled" />
+      <MenuItem label="Share via Facebook" value="facebook" leftIcon="facebookFilled" leftIconStyles={{color: '#3c5a99'}} />
+      <MenuItem label="Share via Twitter" value="twitter" leftIcon="twitterFilled" leftIconStyles={{color: '#1da1f2'}}/>
+    </DropdownMenu>
+  </div>
+  <div>
+    <DropdownMenu triggerElement={
+      <Button> <Icon name="shareAndroid2Filled"/> </Button>
+    }>
+      <MenuItem label="Share via Email" value="email" leftIcon="emailFilled" />
+      <MenuItem label="Share via Facebook" value="facebook" leftIcon="facebookFilled" leftIconStyles={{color: '#3c5a99'}} />
+      <MenuItem label="Share via Twitter" value="twitter" leftIcon="twitterFilled" leftIconStyles={{color: '#1da1f2'}}/>
+    </DropdownMenu>
+  </div>
+</div>
+`}
+</Playground>
+</Fragment>
+
+)
---
+++ site/src/components/docs/MenuItem.js
@@ -0,0 +1,10 @@
+import React, {Fragment} from 'react'
+import Playground from '../Playground'
+
+export default () => (
+
+  <Fragment>
+    MenuItem should always be wrapped in parent Menu or DropdownMenu. And onClick should be accessed through the parent's onSelect callback.
+  </Fragment>
+
+)
