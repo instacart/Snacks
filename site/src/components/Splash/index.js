c0eb4dc3058f8df8a36f9762e7f37e7a1dc682d0
spencerschack <spencer.schack@instacart.com>
2018-06-28 17:04:02 -0700

    buttons

--- site/src/components/Splash/index.js
+++ site/src/components/Splash/index.js
@@ -1,5 +1,5 @@
 import React, {Fragment} from 'react'
-import {spacing} from 'ic-snacks'
+import {Button, spacing} from 'ic-snacks'
 import {Title1, Subtitle} from '../Typography'
 import splash from './splash.png'
 import * as styles from './styles'
@@ -16,6 +16,25 @@ export default function Splash() {
         pieces, designed for web, iOS, and Android. Paired with them, you will
         find React components. Enjoy!
       </Subtitle>
+      <div style={{display: 'flex', marginTop: spacing.XL}}>
+        <Button
+          size="large"
+          href="https://github.com/instacart/Snacks"
+          style={{textDecoration: 'none'}}
+          elementAttributes={{target: '_blank'}}
+          >
+          GitHub
+        </Button>
+        <Button
+          size="large"
+          href="https://www.npmjs.com/package/ic-snacks"
+          style={{textDecoration: 'none'}}
+          snacksStyle="flat"
+          elementAttributes={{target: '_blank'}}
+        >
+          npm
+        </Button>
+      </div>
       <img src={splash} style={styles.splash} alt="teamwork carrot" />
     </Fragment>
   )
