1ec4c7f5ca82d4bb46f5026d016606d50df75c70
Brandon Lawrence <brandon.lawrence@instacart.com>
2018-08-15 09:49:21 -0700

    allow snacksTheme prop to be overridden (#196)
    * allow snacksTheme prop to be overridden
    
    we also throw in development to ensure devs notice mistakes quickly
    
    s
    
    * use existing cleanConfig util for snacksTheme verification

--- src/styles/themer/withTheme.js
+++ src/styles/themer/withTheme.js
@@ -2,7 +2,7 @@
 // and passes down theme through props
 import React, { Component } from 'react'
 import themer from './index'
-import { themePropTypes } from './utils'
+import { cleanConfig, themePropTypes } from './utils'
 
 const isStateless = (Component) => {
   return !Component.prototype.render
@@ -18,10 +18,26 @@ function withTheme(InnerComponent) {
 
     componentDidMount() {
       this.unsubscribe = themer.subscribe(this.onThemeChange)
+      this.validateSnacksTheme()
     }
 
     componentWillUnmount() {
       this.unsubscribe()
+    }
+
+    themeIsValid() {
+      const { snacksTheme } = this.props
+      return Boolean(snacksTheme) && typeof snacksTheme === 'object'
+    }
+
+    validateSnacksTheme() {
+      if (__DEV__) { // eslint-disable-line no-undef
+        const { snacksTheme } = this.props
+        const themeIsBad = snacksTheme && !Object.keys(cleanConfig(snacksTheme)).length
+        if (themeIsBad) {
+          throw new Error(`Recieved an invalid snacksTheme Prop. Expected undefined or an object and instead got ${typeof snacksTheme}`)
+        }
+      }
     }
 
     onThemeChange = () => {
@@ -30,12 +46,14 @@ function withTheme(InnerComponent) {
 
     render() {
       const getRef = (node) => this.wrapped = node
+      const { snacksTheme, ...rest } = this.props
+      const theme = this.themeIsValid() ? snacksTheme : themer.themeConfig
 
       return (
         <InnerComponent
           ref={isStateless(InnerComponent) ? undefined : getRef}
-          snacksTheme={themer.themeConfig}
-          {...this.props}
+          snacksTheme={theme}
+          {...rest}
         />
       )
     }
