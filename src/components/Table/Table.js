22994e87d9ea5af97573b319d0beb662b35c2f63
Tim Marshall <tim@marshall.io>
2018-09-17 17:31:40 -0700

    use styles

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,118 +1,115 @@
-import React      from 'react'
-import PropTypes  from 'prop-types'
-import Radium     from 'radium'
+import React               from 'react'
+import PropTypes           from 'prop-types'
+import Radium              from 'radium'
+import { spacing, colors } from '../../styles'
 
 const styles = {
   table: {
     borderCollapse: 'collapse',
     borderRadius: '4px',
     borderSpacing: 0,
-    boxShadow: '0 1px 2px #aaa',
+    boxShadow: '0 1px 2px #AAA',
     overflow: 'hidden',
     width: '100%',
   },
 
   cell: {
-    background: '#fff',
+    background: colors.WHITE,
     fontSize: '14px',
     fontWeight: 400,
-    height: '48px',
-    lineHeight: '48px',
-    padding: '0 16px',
+    height: spacing.XL,
+    lineHeight: `${spacing.XL}px`,
+    padding: `0 ${spacing.SM}px`,
     textAlign: 'left',
     whiteSpace: 'nowrap',
   },
 
   cellAlt: {
-    background: '#f7f7f7',
+    background: colors.GRAY_97,
   },
 
   clickable: {
     cursor: 'pointer',
     // ':hover': {
-    //   background: '#feffe8',
+    //   background: '#FEFFE8',
     // }
   }
 }
 
-@Radium
-class Table extends React.PureComponent {
-  static propTypes = {
-    data: PropTypes
-      .arrayOf(PropTypes.object)
-      .isRequired,
+const Table = props => {
+  const { withHeader, definition, onRowClick, data } = props
 
-    definition: (props, propName) => {
-      const definition = props[propName]
+  return (
+    <table style={styles.table}>
+      {!withHeader ? null : (
+        <thead>
+          <tr>
+            {definition.map((def, index) => (
+              <th key={index} style={styles.cell}>
+                {def.header !== undefined ? def.header : def.attribute}
+              </th>
+            ))}
+          </tr>
+        </thead>
+      )}
 
-      if (!Array.isArray(definition)) {
-        return new Error('Table required definition prop must be an Array')
-      }
+      <tbody>
+        {data.map((row, index) => (
+          <tr key={index}>
+            {definition.map((def, cellIndex) => {
+              const cellStyles = [styles.cell]
+              if (index % 2 === 0) {
+                cellStyles.push(styles.cellAlt)
+              }
 
-      for (const cell of definition) {
-        if (cell.attribute === undefined) {
-          return new Error('All Table definition object must have an .attribute')
-        }
-      }
-    },
+              const clickHandler = def.onClick || onRowClick
+              if (clickHandler) {
+                cellStyles.push(styles.clickable)
+              }
 
-    withHeader: PropTypes.bool,
-  }
-
-  static defaultProps = {
-    withHeader: true,
-  }
-
-  render() {
-
-    const { withHeader, definition, onRowClick, data } = this.props
-
-    return (
-      <table style={styles.table}>
-        {!withHeader ? null : (
-          <thead>
-            <tr>
-              {definition.map((def, index) => (
-                <th key={index} style={styles.cell}>
-                  {def.header !== undefined ? def.header : def.attribute}
-                </th>
-              ))}
-            </tr>
-          </thead>
-        )}
-
-        <tbody>
-          {data.map((row, index) => (
-            <tr key={index}>
-              {definition.map((def, cellIndex) => {
-                const cellStyles = [styles.cell]
-                if (index % 2 === 0) {
-                  cellStyles.push(styles.cellAlt)
-                }
-
-                const clickHandler = def.onClick || onRowClick
-                if (clickHandler) {
-                  cellStyles.push(styles.clickable)
-                }
-
-                return (
-                  <td
-                    key={cellIndex}
-                    style={cellStyles}
-                    onClick={!clickHandler ? null : () => {
-                      clickHandler(row)
-                    }}
-                  >
-                    {def.cellRender ? def.cellRender(row[def.attribute], index, row) : row[def.attribute]}
-                  </td>
-                )
-              })}
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    )
-  }
+              return (
+                <td
+                  key={cellIndex}
+                  style={cellStyles}
+                  onClick={!clickHandler ? null : () => {
+                    clickHandler(row)
+                  }}
+                >
+                  {def.cellRender ? def.cellRender(row[def.attribute], index, row) : row[def.attribute]}
+                </td>
+              )
+            })}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
 }
 
-export default Table
+Table.propTypes = {
+  data: PropTypes
+    .arrayOf(PropTypes.object)
+    .isRequired,
+
+  definition: (props, propName) => {
+    const definition = props[propName]
+
+    if (!Array.isArray(definition)) {
+      return new Error('Table required definition prop must be an Array')
+    }
+
+    for (const cell of definition) {
+      if (cell.attribute === undefined) {
+        return new Error('All Table definition object must have an .attribute')
+      }
+    }
+  },
+
+  withHeader: PropTypes.bool,
+}
+
+Table.defaultProps = {
+  withHeader: true,
+}
+
+export default Radium(Table)
