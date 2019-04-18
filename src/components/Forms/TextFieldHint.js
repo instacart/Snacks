02c6c5a24625d981f46468267ec078c6c2220d0a
Young Choi <young.choi@instacart.com>
2019-04-17 18:13:51 -0700

    Updating MaskedTextField and hint for accessibility. Adding DateField example

--- src/components/Forms/TextFieldHint.js
+++ src/components/Forms/TextFieldHint.js
@@ -15,9 +15,11 @@ const styles = {
     transition: 'opacity 250ms cubic-bezier(0.23, 1, 0.32, 1) 0ms',
     pointerEvents: 'none',
     zIndex: 1,
+    visibility: 'hidden',
   },
   show: {
     opacity: 1,
+    visibility: 'visible',
   },
   disabled: {
     cursor: 'not-allowed',
@@ -35,6 +37,8 @@ class TextFieldHint extends Component {
     show: PropTypes.bool,
     /** Override styles */
     style: PropTypes.object,
+    /** A uniq id */
+    inputId: PropTypes.string,
   }
 
   static defaultProps = {
@@ -42,10 +46,13 @@ class TextFieldHint extends Component {
   }
 
   render() {
-    const { disabled, show, style, text } = this.props
+    const { disabled, show, style, text, inputId } = this.props
 
     return (
-      <div style={[styles.root, style, disabled && styles.disabled, show && styles.show]}>
+      <div
+        id={`hint_${inputId}`}
+        style={[styles.root, style, disabled && styles.disabled, show && styles.show]}
+      >
         {text}
       </div>
     )
---
+++ src/components/Forms/docs/DateField.md
@@ -0,0 +1,14 @@
+DateField Example:
+```js
+<div style={{ marginBottom: 10 }}>
+  <DateField
+    name='dob'
+    type='text'
+    floatingLabelText='Date of birth'
+    required
+    halfWidth
+    id="dob"
+    validationErrorText="Please enter date of birth"
+  />
+</div>
+```
