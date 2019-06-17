e3afd4e4d21a671c99da5c3f67da929222970ba8
Ksenija Gogic <ksenija.gogic@instacart.com>
2019-06-17 14:01:58 -0400

    Reverts markup changes

--- src/components/Tooltip/InnerToolTip.js
+++ src/components/Tooltip/InnerToolTip.js
@@ -23,12 +23,6 @@ const styles = {
   },
 }
 
-const spanStyles = {
-  root: {
-    position: 'relative',
-  },
-}
-
 const RESOLVED_COLOR = {
   primary: {
     background: colors.GREEN_500,
@@ -113,10 +107,8 @@ class InnerToolTip extends PureComponent {
   render() {
     return (
       <Fade>
-        <div style={this.contentStyles}>
-          {this.renderArrow()}
-          <span style={spanStyles.root}>{this.props.children}</span>
-        </div>
+        {this.renderArrow()}
+        <div style={this.contentStyles}>{this.props.children}</div>
       </Fade>
     )
   }
