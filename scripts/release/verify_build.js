127a2a4812426b035cd8bde23e3b9b9db10eba3e
Nathan Marks <nathan.marks@instacart.com>
2019-06-26 12:07:32 -0400

    Add SVG component type declaration generation and verification

--- scripts/release/verify_build.js
+++ scripts/release/verify_build.js
@@ -4,23 +4,52 @@
 // This also ensures that Snacks can be imported into a node environment
 
 // basic importing works (most issues will appear here)
-import * as Snacks from '../../dist/snacks'
+import { execSync } from 'child_process'
+import * as SnacksUMD from '../../dist/snacks'
 import colors from '../../src/styles/colors'
 import zIndex from '../../src/styles/zIndex'
 
-// verify colors from dist equal src
-Object.keys(Snacks.colors).forEach((color) => {
-  if (colors[color] !== Snacks.colors[color]) {
-    throw new Error(`Snacks dist file colors do not match - expect ${colors[color]}, but saw ${Snacks.colors[color]}`)
-  }
-})
+const exec = command => execSync(command, { stdio: 'inherit' })
 
-// verify zIndex from dist equal src
-Object.keys(Snacks.zIndex).forEach((zValue) => {
-  const sourceObj = zIndex[zValue]
-  const builtObj = Snacks.zIndex[zValue]
+function verifyBuild(SnacksBuild) {
+  // verify colors from dist equal src
+  Object.keys(SnacksBuild.colors).forEach(color => {
+    if (colors[color] !== SnacksBuild.colors[color]) {
+      throw new Error(
+        `Snacks dist file colors do not match - expect ${colors[color]}, but saw ${
+          SnacksBuild.colors[color]
+        }`
+      )
+    }
+  })
 
-  if (sourceObj.zIndex !== builtObj.zIndex) {
-    throw new Error(`Snacks dist file zIndex do not match - expect ${sourceObj.zIndex}, but saw ${builtObj.zIndex}`)
-  }
-})
+  // verify zIndex from dist equal src
+  Object.keys(SnacksBuild.zIndex).forEach(zValue => {
+    const sourceObj = zIndex[zValue]
+    const builtObj = SnacksBuild.zIndex[zValue]
+
+    if (sourceObj.zIndex !== builtObj.zIndex) {
+      throw new Error(
+        `Snacks dist file zIndex do not match - expect ${sourceObj.zIndex}, but saw ${
+          builtObj.zIndex
+        }`
+      )
+    }
+  })
+}
+
+console.log('Verifying UMD build')
+
+verifyBuild(SnacksUMD)
+
+console.log('Verifying ESM build (bundling and importing)')
+
+exec('yarn webpack --config=webpack.verify-esm.config.js')
+
+const SnacksESMBundle = require('../../tmp/esm-bundle')
+
+verifyBuild(SnacksESMBundle)
+
+console.log('Verifying typings')
+
+exec('yarn tsc -p tsconfig.verify-build.json')
