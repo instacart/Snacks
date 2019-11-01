724c51af75df457cd56be81029f2a68b793c84a4
Stefanie Contreras <stefceror@users.noreply.github.com>
2019-11-01 11:51:11 -0700

    Add radio group type export (#381)
    * Add radio group type export
    
    * Actually export it

--- src/index.d.ts
+++ src/index.d.ts
@@ -5,6 +5,7 @@ import Tooltip from './components/Tooltip/Tooltip'
 import Button from './components/Buttons/Button'
 import CircleButton from './components/Buttons/CircleButton'
 import Radio from './components/Buttons/Radio'
+import RadioGroup from './components/Buttons/RadioGroup'
 import Checkbox from './components/Buttons/Checkbox'
 import Switch from './components/Buttons/Switch'
 import Column from './components/Grid/Column'
@@ -123,6 +124,7 @@ export {
   Button,
   CircleButton,
   Radio,
+  RadioGroup,
   Checkbox,
   Switch,
   Icon,
